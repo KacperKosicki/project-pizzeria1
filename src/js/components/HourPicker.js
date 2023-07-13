1b53b6dfdbb198a8f4d70b3738a5bf6b00b5b4e9
Kacper Kosicki <kosickikacper1@gmail.com>
2023-07-13 18:44:18 +0200

    Add thisWidget.setValue(thisWidget.value); line of code that adds the hour above the slider depending on the scrolling

--- src/js/components/HourPicker.js
+++ src/js/components/HourPicker.js
@@ -19,6 +19,7 @@ class HourPicker extends BaseWidget{
     rangeSlider.create(thisWidget.dom.input);
     thisWidget.dom.input.addEventListener('input', function(){
       thisWidget.value = thisWidget.dom.input.value;
+      thisWidget.setValue(thisWidget.value);
     });
   }
 
