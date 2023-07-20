2d67ba99f0a0f520728898c34a91f278f901a915
Kacper Kosicki <kosickikacper1@gmail.com>
2023-07-20 21:19:19 +0200

    Add a homepage to the project. Using Handlebars for the Home page and add the Home.js class. Adding _home.scss and its configuration

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -1,123 +1,113 @@
 class Home {
-    constructor() {
-      this.container = document.querySelector('#home-wrapper');
-      this.render();
-      this.initCarousel();
-      this.addEventListeners();
-    }
-  
-    render() {
-        this.renderBoxes();
-        this.renderCarousel();
-        this.renderGallery();
-      }
-      
-      renderBoxes() {
-        const boxesContent = `
-          <div class="boxes">
-            <div class="box order-box">
-              <h2>Order Online</h2>
-              <p>Place an online order and enjoy our delicious pizzas!</p>
-            </div>
-            <div class="box booking-box">
-              <h2>Book a Table</h2>
-              <p>Reserve a table and have a great dining experience!</p>
-            </div>
-            <div class="box opening-box">
-              <h2>Opening Hours</h2>
-              <p>Monday - Friday: 9am - 10pm<br>Saturday - Sunday: 10am - 11pm</p>
-            </div>
-          </div>
-        `;
-        this.container.innerHTML = boxesContent;
-      }
-      
-      renderCarousel() {
-        const carouselContent = `
-          <div class="carousel">
-            <div class="carousel-item active">
-              <blockquote>
-                <p>Great pizza, amazing service!</p>
-                <footer>- John Doe</footer>
-              </blockquote>
-            </div>
-            <div class="carousel-item">
-              <blockquote>
-                <p>Delicious and authentic Italian flavors!</p>
-                <footer>- Jane Smith</footer>
-              </blockquote>
-            </div>
-            <div class="carousel-item">
-              <blockquote>
-                <p>Best pizza in town, hands down!</p>
-                <footer>- Mike Johnson</footer>
-              </blockquote>
-            </div>
-          </div>
-        `;
-        this.container.innerHTML += carouselContent;
-      }
-      
-      renderGallery() {
-        const galleryContent = `
-          <div class="gallery">
-            <div class="gallery-item">
-              <div class="overlay">
-                <i class="fas fa-search"></i>
-              </div>
-            </div>
-            <div class="gallery-item">
-              <div class="overlay">
-                <i class="fas fa-search"></i>
-              </div>
-            </div>
-            <div class="gallery-item">
-              <div class="overlay">
-                <i class="fas fa-search"></i>
-              </div>
-            </div>
-          </div>
-        `;
-        this.container.innerHTML += galleryContent;
-      }
-  
-    initCarousel() {
-      const carouselElement = document.querySelector('.carousel');
-      new Flickity(carouselElement, {
-        autoPlay: true,
-        wrapAround: true,
-      });
-    }
-  
-    addEventListeners() {
-      const orderBox = document.querySelector('.order-box');
-      const bookingBox = document.querySelector('.booking-box');
-  
-      orderBox.addEventListener('click', () => {
-        window.location.href = 'order.html';
-      });
-  
-      bookingBox.addEventListener('click', () => {
-        // Redirect to the Booking page
-        window.location.href = 'booking.html';
-      });
-  
-      const galleryItems = document.querySelectorAll('.gallery-item');
-      galleryItems.forEach((item) => {
-        item.addEventListener('mouseenter', () => {
-          // Animate the text on hover
-          item.classList.add('animate');
-        });
-  
-        item.addEventListener('mouseleave', () => {
-          // Remove the text animation
-          item.classList.remove('animate');
-        });
-      });
-    }
+  constructor() {
+    this.container = document.querySelector('#home .home-wrapper');
+    this.render();
+    this.initCarousel();
   }
-  
-  // Create an instance of Home class
-  const home = new Home();
 
-  export default Home;
+  render() {
+    const context = {
+      boxes: [
+        {
+          class: 'order-box',
+          title: 'Order Online',
+          description: 'Place an online order and enjoy our delicious pizzas!',
+          backgroundImage: 'images/home/pizza-background.jpg',
+          showOrderOnlineLabel: true
+        },
+        {
+          class: 'booking-box',
+          title: 'Book a Table',
+          description: 'Reserve a table and have a great dining experience!',
+          backgroundImage: 'images/home/booking-background.jpg'
+        },
+        {
+          class: 'opening-box',
+          title: 'OPENING HOURS',
+          description: 'TUE-SUN, 12PM - 12AM',
+          backgroundImage: '-'
+        }
+      ],
+      carouselItems: [
+        {
+          quote: 'Great pizza, amazing service!',
+          author: 'John Doe',
+          authorImage: 'images/home/man1.jpg', // Ścieżka do zdjęcia autora
+          authorDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Opis autora
+          active: true,
+        },
+        {
+          quote: 'Amazing service!',
+          author: 'Jess Erdem',
+          authorImage: 'images/home/man2.jpg', // Ścieżka do zdjęcia autora
+          authorDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Opis autora
+          active: true,
+        },
+        {
+          quote: 'Great pizza!',
+          author: 'Susanna Stan',
+          authorImage: 'images/home/woman1.jpg', // Ścieżka do zdjęcia autora
+          authorDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Opis autora
+          active: true,
+        },
+      ],
+      galleryItems: [
+        'images/home/restaurant1.jpg',
+        'images/home/restaurant2.jpg',
+        'images/home/restaurant3.jpg',
+        'images/home/restaurant4.jpg',
+        'images/home/restaurant5.jpg',
+        'images/home/restaurant6.jpg',
+      ],
+    };
+
+    const templateSource = document.getElementById('template-home').innerHTML;
+    const template = Handlebars.compile(templateSource);
+    this.container.innerHTML = template(context);
+
+    this.initCarousel();
+  }
+
+  initCarousel() {
+    const carouselElement = this.container.querySelector('.carousel');
+    // eslint-disable-next-line no-undef
+    const flkty = new Flickity(carouselElement, {
+      autoPlay: true,
+      wrapAround: true,
+      prevNextButtons: false,
+    });
+  
+    flkty.on('select', () => {
+      const allSlides = carouselElement.querySelectorAll('.carousel-item');
+      allSlides.forEach((slide, index) => {
+        slide.classList.toggle('carousel-item-hidden', index !== flkty.selectedIndex);
+      });
+    });
+  }
+
+  addEventListeners() {
+    const orderBox = document.querySelector('.order-box');
+    const bookingBox = document.querySelector('.booking-box');
+
+    orderBox.addEventListener('click', () => {
+      window.location.href = 'order.html';
+    });
+
+    bookingBox.addEventListener('click', () => {
+      window.location.href = 'booking.html';
+    });
+
+    const galleryItems = document.querySelectorAll('.gallery-item');
+    galleryItems.forEach((item) => {
+      item.addEventListener('mouseenter', () => {
+        item.classList.add('animate');
+      });
+
+      item.addEventListener('mouseleave', () => {
+        item.classList.remove('animate');
+      });
+    });
+  }
+}
+
+export default Home;
